a06ad01f1151901fa37a101854373f1056cbdcf6
MinhQuan-Github <minhquan.5322@aol.com>
2022-01-23 10:12:54 +0700

    initial commit

---
+++ Dicee (swiftUI)/Dicee__swiftUI_App.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee__swiftUI_App.swift
+//  Dicee (swiftUI)
+//
+//  Created by Quan's Macbook on 23/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct Dicee__swiftUI_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
